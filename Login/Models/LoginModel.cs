df2fe985c2ad7dd7d80ff503c13669fe9793a866
Jeslin-C-John <111417450+Jeslin-C-John@users.noreply.github.com>
2022-12-05 18:50:27 +0530

    Add project files.

---
+++ Login/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Login.Models
+{
+    public class LoginModel
+    {
+        public int name { get; set; }
+        public string password { get; set; }
+
+    }
+}
---
+++ Login/Models/userDbHandle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Login.Models
+{
+    public class userDbHandle
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["UserConn"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        public void adduser(LoginModel insert)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("InsertUser", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Name", insert.name);
+            cmd.Parameters.AddWithValue("@Password", insert.password);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
